43de17bdd55ec0ec309f5c2a94d0fbd688551eaf
Dmitry B <lbvf6000@gmail.com>
2021-01-20 22:38:34 +0300

    completed day1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 5;
+        for (int count = 1; count < 10; count++) {
+            int result = count * k;
+            System.out.println(count+" x "+k+" = "+result);
+        }
     }
 }
