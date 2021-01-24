f29ad2ee40cf79b30a37feb71c353105c8e38e54
Dmitry B <lbvf6000@gmail.com>
2021-01-24 13:46:04 +0300

    Day 2 Task 1-3

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,14 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+            else while()
     }
 }
