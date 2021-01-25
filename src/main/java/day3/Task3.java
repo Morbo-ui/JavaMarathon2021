2fe20de05e1589a430e6cd13897db0d53d16a652
Dmitry <lbvf6000@gmail.com>
2021-01-25 23:52:34 +0300

    Day 3, tasks 1-3 done

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double delimoe = scanner.nextDouble();
+            double delitel = scanner.nextDouble();
+            double res = delimoe / delitel;
+            if (delitel == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(res);
+        }
     }
 }
+
