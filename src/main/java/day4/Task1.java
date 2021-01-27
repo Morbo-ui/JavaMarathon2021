3b9429980020aef66a30fc1e2021629887589977
Dmitry B <lbvf6000@gmail.com>
2021-01-27 18:04:27 +0300

    Start day-4

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,21 @@
 package day4;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int [] numbers = new int [n];
+        for (int i = 0; i <n ; i++) {
+            numbers = Math.random()*10
+        }
+         numbers = Math.random()*10
+        for (int x:numbers){
+            System.out.println(n);
+            System.out.println(x > 8);
+            System.out.println(x == 1);
 
+        }
     }
 }
