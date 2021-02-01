8c931d5aa80d461ad37935017a5c32592f476dfa
Dmitry <lbvf6000@gmail.com>
2021-02-01 15:12:19 +0300

    complete day 4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -7,8 +7,21 @@ public class Task4 {
         int[] array = new int[100];
         Random rand = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = rand.nextInt();
+            array[i] = rand.nextInt(10000);
         }
-
+        int sum = 0;
+        int max = 0;
+        int maxSumId = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            for (int j = 0; j < i; j++) {
+                sum += array[j];
+            }
+            if (sum > max) {
+                max = sum;
+                maxSumId = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(maxSumId);
     }
 }
