79cd4dbddec2522696f556fcb8685121c49c9b72
Dmitry B <lbvf6000@gmail.com>
2021-02-02 19:25:50 +0300

    start Day6

---
+++ src/main/java/day6/Moto.java
@@ -0,0 +1,40 @@
+package day6;
+
+
+public class Moto {
+    private int year;
+    private String color;
+    private String model;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    void info() {
+        System.out.println("Это мотоцикл");
+    }
+
+    public int yearDifference(int inputYear) {
+        return Math.abs(inputYear - year);
+    }
+}
--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -2,6 +2,17 @@ package day6;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setColor("yellow");
+        car.setModel("Buggati");
+        car.setYear(1984);
+        car.info();
+        System.out.println(car.yearDifference(2020));
+        Moto moto = new Moto();
+        moto.setModel("Ducati");
+        moto.setColor("Pink");
+        moto.setYear(1999);
+        moto.info();
+        System.out.println(moto.yearDifference(2021));
     }
 }
