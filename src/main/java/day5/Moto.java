7abc40a8827921f1dde9ddc7d47b80bba617887d
Dmitry B <lbvf6000@gmail.com>
2021-02-02 17:21:25 +0300

    Day 5 finish

--- src/main/java/day5/Moto.java
+++ src/main/java/day5/Moto.java
@@ -4,5 +4,21 @@ public class Moto {
     private int year;
     private String color;
     private String model;
-    
+
+    public Moto(String color, String model, int year) {
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,7 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Moto moto = new Moto("black", "Nissan", 2012 );
+        System.out.println(moto.getYear() + "\n" + moto.getModel() + "\n" + moto.getColor());
     }
 }
