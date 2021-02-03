cdd6e9cf18fcd94d28304c88bf42459fb79de170
Dmitry B <lbvf6000@gmail.com>
2021-02-03 21:24:18 +0300

    Start day6, task 3

--- src/main/java/day6/Task3.java
+++ src/main/java/day6/Task3.java
@@ -2,6 +2,8 @@ package day6;
 
 public class Task3 {
     public static void main(String[] args) {
-
+    Teacher teacher = new Teacher("Vladimir Vladimirovich", "Maths");
+    Student student = new Student ("Leha");
+    teacher.evaluate();
     }
 }
--- src/main/java/day6/Teacher.java
+++ src/main/java/day6/Teacher.java
@@ -1,5 +1,14 @@
 package day6;
 
 public class Teacher {
-    
+    private String name;
+    private String subject;
+
+    public Teacher(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+    public void evaluate(Student){
+
+    }
 }
