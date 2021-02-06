8f1e208d1f60790157ca4b0fc3220c3493e4ff83
Dmitry B <lbvf6000@gmail.com>
2021-02-06 10:14:55 +0300

    day 8 finish (StringBuilder)

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,30 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String s = "";
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++)
+            s += i + " ";
+
+
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+
+        System.out.println("String " + timeConsumedMillis);
+        System.out.println(s);
+
+
+        StringBuilder sb = new StringBuilder("");
+         start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++)
+            sb.append(i).append(" ");
+
+         finish = System.currentTimeMillis();
+         timeConsumedMillis = finish - start;
+        System.out.println(" StringBuilder :" + timeConsumedMillis);
+
+        System.out.println(sb);
+
 
     }
 }
