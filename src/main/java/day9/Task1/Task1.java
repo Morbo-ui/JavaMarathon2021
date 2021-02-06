23523a86bb50eb2a348a8ca981c095cf61dd33c0
Dmitry B <lbvf6000@gmail.com>
2021-02-06 18:08:21 +0300

    day 9 finish task 1

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,10 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Леха","БПИ");
+        Teacher teacher = new Teacher("Владимир Александрович","Математка");
+        student.printInfo();
+        teacher.printInfo();
 
     }
 }
