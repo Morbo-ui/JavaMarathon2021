579338980247e5f2a6175a4904b25da75c0e25ea
Dmitry B <lbvf6000@gmail.com>
2021-02-15 12:59:47 +0300

    day 12 start

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,19 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        List<String> brandCars = new ArrayList<>();
+        brandCars.add("Buggati");
+        brandCars.add("Tesla");
+        brandCars.add("Toyota");
+        brandCars.add("Nissan");
+        brandCars.add("Ford");
+        brandCars.add(2,"APPLE");
+        brandCars.remove(0);
+        System.out.println(brandCars);
     }
 }
