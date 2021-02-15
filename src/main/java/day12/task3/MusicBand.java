29cf50bdc27077368dc16f35648dff5eaed0ff3f
Dmitry <lbvf6000@gmail.com>
2021-02-15 16:04:33 +0300

    Day 12 start

---
+++ src/main/java/day12/task3/MusicBand.java
@@ -0,0 +1,14 @@
+package day12.task3;
+
+public class MusicBand {
+    private int name;
+    private int year;
+
+    public MusicBand(int name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+    public int getYear() {
+        return year;
+    }
+}
--- src/main/java/day12/task3/Task3.java
+++ src/main/java/day12/task3/Task3.java
@@ -2,6 +2,18 @@ package day12.task3;
 
 public class Task3 {
     public static void main(String[] args) {
+        MusicBand musicBand1 = new MusicBand("Nirvanna", 2010);
+        MusicBand musicBand2 = new MusicBand("Metallica", 2000);
+        MusicBand musicBand3 = new MusicBand("Sask", 2010);
+        MusicBand musicBand4 = new MusicBand("mk", 2000);
+        MusicBand musicBand5 = new MusicBand("LMFAO", 2010);
+        MusicBand musicBand6 = new MusicBand("Note", 1880);
+        MusicBand musicBand7 = new MusicBand("Key", 1700);
+        MusicBand musicBand8 = new MusicBand("Orekh", 1990);
+        MusicBand musicBand9 = new MusicBand("Slawa Marlow", 2010);
+        MusicBand musicBand10 = new MusicBand("Morgenshtern", 1990);
+
+
 
     }
 }
