415724c2d4020cbb7e33f2ffc7dec4d8409e48ce
Dmitry B <lbvf6000@gmail.com>
2021-03-09 13:32:46 +0300

    day 14 finish

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,34 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("text");
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+
+            if (numbers.length != 10) {
+                throw new IllegalArgumentException();
+
+            }
+            int sum = 0;
+            for (String mumber : numbers) {
+                sum += Integer.parseInt(mumber);
+            }
+            System.out.println(sum);
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Innapropriate input");
+        }
 
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,43 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(parseFIleToStringList());
+    }
+
+    public static List<String> parseFIleToStringList() {
+        File file = new File("pople");
+        List<String> people = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] person = line.split(" ");
+                if (Integer.parseInt(person[1]) < 0)
+                    throw new IllegalArgumentException();
+
+
+
+                people.add(line);
+
+            }
+            return people;
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файла-то нет, дебил");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Po vozrastu oshibka");
+        }
+        return null;
+
 
     }
 }
